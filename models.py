005d88ecbea3787ad2f07251bf30efe31c2c7096
andrerubeis <78568240+andrerubeis@users.noreply.github.com>
2021-07-13 16:23:53 +0200

    Update models.py
    aggiunta funzione train_newtork_lower_bound da branch di Marco

--- models.py
+++ models.py
@@ -50,6 +50,33 @@ def train_network(classes, model, old_model, optimizer, data_loader, scheduler, 
               str(loss.item()) + ', Accuracy: ' + str(accuracy))
     return model
 
+def train_network_lower_bound(classes, model, optimizer, data_loader, scheduler, task):
+    for epoch in range(params.NUM_EPOCHS):
+        length = 0
+        running_corrects = 0
+        for images, labels, _ in data_loader:
+            images = images.float().to(params.DEVICE)
+            labels = labels.long().to(params.DEVICE)  # .long()
+            onehot_labels = torch.eye(params.NUM_CLASSES)[labels].to(params.DEVICE)
+            mapped_labels = utils.map_splits(labels, classes)
+            optimizer.zero_grad()
+            # features=False : use fully connected layer (see ResNet)
+            # old_outputs = old_model(images, features=False)
+            outputs = model(images, features=False)
+            criterion = torch.nn.BCEWithLogitsLoss()
+            loss = criterion(outputs, onehot_labels)
+            # cut_outputs take only the first #task outputs: see simplification in main
+            cut_outputs = np.take_along_axis(outputs.to(params.DEVICE), classes[None, :], axis=1).to(params.DEVICE)
+            _, preds = torch.max(cut_outputs.data, 1)
+            running_corrects += torch.sum(preds == mapped_labels.data).data.item()
+            length += len(images)
+            loss.backward()
+            optimizer.step()
+        accuracy = running_corrects / float(length)
+        scheduler.step()
+        print('Step: ' + str(task) + ", Epoch: " + str(epoch) + ", Loss: " +
+              str(loss.item()) + ', Accuracy: ' + str(accuracy))
+    return model
 
 def trainLWF(task, train_loader, train_splits):
     print(f'task = {task} ')
